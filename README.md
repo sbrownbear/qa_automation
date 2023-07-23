9c81654bb1a1374794fe8fb8a0f1cd49317b8420
Sergey Konoplev <sergey.gsarh@gmail.com>
2023-07-23 22:02:09 +0300

    added my first java selenide test

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# qa_automation
+## Практика автоматизации
+
+`TextBoxTests.java` Первый тест на сайте [demoqa.com/text-box](https://demoqa.com/text-box). Java, Selenide, Gradle
+
+`JavaSelenideSnippets.java` Команды Selenide
+
+`LocatorExamples.java` Примеры локаторов
+
+`AnnotationExamples.java` Примеры аннотаций
---
+++ TextBoxTests.java
@@ -0,0 +1,42 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TextBoxTests {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = false;
+    }
+
+    @Test
+    void fillFormTest() {
+        String userName = "Sergey Konoplev";
+        String userEmail = "sergeyKonoplev@gmail.com";
+        String currentAddress = "Russia";
+        String permanentAddress = "Penza, ul. Pushkina, dom Kolotushkina";
+
+        open("/text-box");
+        $(".main-header").shouldHave(text("Text Box"));
+
+        $("#userName").setValue(userName);
+        $("#userEmail").setValue(userEmail);
+        $("#currentAddress").setValue(currentAddress);
+        $("#permanentAddress").setValue(permanentAddress);
+        $("#submit").click();
+
+        $("#output").shouldBe(visible);
+        $("#output").$("#name").shouldHave(text(userName));
+        $("#output #email").shouldHave(text(userEmail));
+        $("#output #currentAddress").shouldHave(text(currentAddress));
+        $("#output #permanentAddress").shouldHave(text(permanentAddress));
+    }
+}
